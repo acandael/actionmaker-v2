e7119d6e9e58cb978900c06584e9b4619e214867
Anthony Candaele <info@anthonycandaele.com>
2024-11-21 11:17:27 +0100

    modify footer

--- src/components/Footer/footer-data.ts
+++ src/components/Footer/footer-data.ts
@@ -1,47 +1,41 @@
 export const footerLinks = [
   {
-    title: "Menu",
+    title: 'Menu',
     links: [
-      "Home",
-      "Teambuildings",
-      "Concepten",
-      "Over ActionMaker",
-      "Contact"
-    ]
+      { name: 'Home', url: '/' },
+      { name: 'Teambuildings', url: '/teambuildings' },
+      { name: 'Concepten', url: '/concepten' },
+      { name: 'Over ActionMaker', url: '/over-actionmaker' },
+      { name: 'Contact', url: '/contact' },
+    ],
   },
   {
-    title: "Teambuildings",
+    title: 'Teambuildings',
     links: [
-      "Scherminitiatie",
-      "Boogschieten",
-      "Laserkleduifschieten",
-      "Sumoworstelen",
-      "Blaaspijpschieten",
-      "Pistoolkruisboogschieten",
-      "Pistoolschieten",
-      "Kruisboogschieten"
-    ]
+      { name: 'Scherminitiatie', url: '/teambuildings/scherminitiatie' },
+      { name: 'Boogschieten', url: '/teambuildings/boogschieten' },
+      { name: 'Laserkleduifschieten', url: '/teambuildings/laserkleduifschieten' },
+      { name: 'Sumoworstelen', url: '/teambuildings/sumoworstelen' },
+      { name: 'Blaaspijpschieten', url: '/teambuildings/blaaspijpschieten' },
+      { name: 'Pistoolkruisboogschieten', url: '/teambuildings/pistoolkruisboogschieten' },
+      { name: 'Pistoolschieten', url: '/teambuildings/pistoolschieten' },
+      { name: 'Kruisboogschieten', url: '/teambuildings/kruisboogschieten' },
+    ],
   },
   {
-    title: "Roofvogelworkshop",
+    title: 'Teambuildings',
     links: [
-      "Smaaktest",
-      "Karabijnschieten",
-      "Biathlonschieten",
-      "Bijlwerpen",
-      "Kunstsmeden",
-      "Djembe",
-      "Chocoladeworkshop",
-      "Indoorlasershooting"
-    ]
+      { name: 'Smaaktest', url: '/teambuildings/smaaktest' },
+      { name: 'Biathlonschieten', url: '/teambuildings/biathlonschieten' },
+      { name: 'Bijlwerpen', url: '/teambuildings/bijlwerpen' },
+      { name: 'Kunstsmeden', url: '/teambuildings/kunstsmeden' },
+      { name: 'Djembe', url: '/teambuildings/djembe' },
+      { name: 'Chocoladeworkshop', url: '/teambuildings/chocoladeworkshop' },
+      { name: 'Indoorlasershooting', url: '/teambuildings/indoor-lasershooting' },
+    ],
   },
   {
-    title: "Concepten",
-    links: [
-      "Concept 1",
-      "Concept 2",
-      "Concept 3",
-      "Concept 4"
-    ]
-  }
+    title: 'Concepten',
+    links: ['Concept 1', 'Concept 2', 'Concept 3', 'Concept 4'],
+  },
 ];
