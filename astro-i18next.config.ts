c80a0b3ccfd1e0c22c30c7d14dc1cea93e29373b
Anthony Candaele <info@anthonycandaele.com>
2024-11-25 14:28:39 +0100

    localize Navbar

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,7 +3,7 @@ import type { AstroI18nextConfig } from 'astro-i18next';
 const config: AstroI18nextConfig = {
   defaultLocale: 'nl',
   locales: ['nl', 'fr', 'de'],
-  namespaces: ['home'],
+  namespaces: ['home', 'navbar'],
   i18nextServer: {
     debug: true,
   },
