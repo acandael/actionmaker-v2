824a4c34a5b7795346755d6277ac38a3e9b0c999
Anthony Candaele <info@anthonycandaele.com>
2024-11-27 19:47:20 +0100

    fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
-
+import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-
 import astroI18next from 'astro-i18next';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    astroI18next(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-
   output: 'server',
   adapter: vercel(),
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: 'viewport',
-  },
-  image: {
-    domains: [],
-    remotePatterns: [],
-    dirs: ['src/content/images/activities'],
-  },
+  integrations: [tailwind(), react(), astroI18next()],
 });
