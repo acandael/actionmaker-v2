ea3b52c1b8fbbf74080e31b6619d053eb9729f76
Anthony Candaele <info@anthonycandaele.com>
2024-11-28 11:47:30 +0100

    fix warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,29 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 import astroI18next from 'astro-i18next';
+import path from 'path';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), react(), astroI18next()],
-  prefetch: {
-    prefetchAll: true,
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(path.dirname(''), './src'),
+        '@components': path.resolve(path.dirname(''), './src/components'),
+        '@assets': path.resolve(path.dirname(''), './src/assets'),
+      },
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom'],
+    },
+    build: {
+      sourcemap: true,
+    },
   },
 });
