8bbd723e10608098f04a7da23af2434d413d6bb2
Anthony Candaele <info@anthonycandaele.com>
2024-11-28 11:11:03 +0100

    fix build issues on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,41 +6,9 @@ import astroI18next from 'astro-i18next';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    analytics: true,
-    webAnalytics: {
-      enabled: true,
-    },
-    devImageService: 'sharp',
-    imagesConfig: {
-      sizes: [320, 640, 1280],
-      domains: [],
-      formats: ['image/avif', 'image/webp'],
-      minimumCacheTTL: 60,
-    },
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), react(), astroI18next()],
   vite: {
-    build: {
-      rollupOptions: {
-        external: [
-          'resend',
-          '@react-email/components',
-          'react-hook-form',
-          '@hookform/resolvers/zod',
-          'lucide-react',
-          'sonner',
-          '@radix-ui/react-slot',
-          'class-variance-authority',
-          '@radix-ui/react-label',
-          'next-themes',
-          '@radix-ui/react-radio-group',
-          '@radix-ui/react-separator',
-          'tailwind-merge',
-          '@radix-ui/react-accordion',
-        ],
-      },
-    },
     ssr: {
       noExternal: [
         '@react-email/components',
@@ -48,14 +16,6 @@ export default defineConfig({
         '@hookform/resolvers/zod',
         'lucide-react',
         'sonner',
-        '@radix-ui/react-slot',
-        'class-variance-authority',
-        '@radix-ui/react-label',
-        'next-themes',
-        '@radix-ui/react-radio-group',
-        '@radix-ui/react-separator',
-        'tailwind-merge',
-        '@radix-ui/react-accordion',
       ],
     },
   },
