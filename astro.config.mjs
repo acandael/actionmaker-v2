8dbf6207a16a166a69740d3e25ff01fc006ee301
Anthony Candaele <info@anthonycandaele.com>
2024-11-29 10:00:34 +0100

    revert back to minimal configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,9 @@ import vercel from '@astrojs/vercel/serverless';
 import astroI18next from 'astro-i18next';
 import { fileURLToPath } from 'url';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    functionPerRoute: true,
-    maxDuration: 60,
-    runtime: 'nodejs20.x',
-    includeFiles: ['**/*'],
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), react(), astroI18next()],
   vite: {
     resolve: {
@@ -26,12 +16,6 @@ export default defineConfig({
         '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
         '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
       },
-    },
-    optimizeDeps: {
-      include: ['react', 'react-dom'],
-    },
-    build: {
-      sourcemap: true,
     },
   },
 });
