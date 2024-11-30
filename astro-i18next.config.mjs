ff276f3a7a9913eef1878506dcd21ffd2c10e2a8
Anthony Candaele <info@anthonycandaele.com>
2024-11-30 11:27:42 +0100

    fix function invocation failed

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,37 +1,19 @@
-const config = {
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
   defaultLocale: 'nl',
   locales: ['nl', 'fr', 'de'],
-  routes: {
-    fr: {
-      'over-actionmaker': 'a-propos-actionmaker',
-      'uber-actionmaker': 'a-propos-actionmaker',
-      teambuildings: 'teambuildings',
-      concepten: 'concepts',
-      contact: 'contact',
-      kontakt: 'contact',
-    },
-    de: {
-      'over-actionmaker': 'uber-actionmaker',
-      'a-propos-actionmaker': 'uber-actionmaker',
-      teambuildings: 'teambuildings',
-      concepten: 'konzepte',
-      contact: 'kontakt',
-      kontakt: 'kontakt',
-    },
-  },
   namespaces: [
     'translation',
     'navbar',
     'activity',
     'booking',
-    'teambuildings',
-    'home',
     'about',
-    'contact',
     'footer',
+    'faq',
+    'home',
+    'teambuildings',
   ],
-  defaultNamespace: 'home',
-  load: ['client', 'server'],
+  defaultNamespace: 'translation',
+  load: ['server'],
   resourcesBasePath: 'src/locales',
 };
-export default config;
