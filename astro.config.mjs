4aa0674d6b0c6ac6a537f9dfe63d458ae53a73d0
Anthony Candaele <info@anthonycandaele.com>
2024-11-30 11:47:36 +0100

    install @astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/edge';
+import vercelEdge from '@astrojs/vercel/edge';
 import astroI18next from 'astro-i18next';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
+  adapter: vercelEdge({
     analytics: true,
     imageService: true,
     imagesConfig: {
