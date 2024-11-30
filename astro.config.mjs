9327cac331b4e69af5d25546eb5463458a293c9e
Anthony Candaele <info@anthonycandaele.com>
2024-11-30 11:50:46 +0100

    revert to serverless configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercelEdge from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 import astroI18next from 'astro-i18next';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercelEdge({
+  adapter: vercel({
     analytics: true,
     imageService: true,
     imagesConfig: {
@@ -15,6 +15,7 @@ export default defineConfig({
       sizes: [320, 640, 1280],
       formats: ['image/avif', 'image/webp'],
     },
+    nodeVersion: '18.x',
     functionPerRoute: true,
     maxDuration: 60,
     assets: [
